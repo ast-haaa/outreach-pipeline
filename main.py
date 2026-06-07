1b0fe915a6280a6af9eb70539640483ad2ed4201
Asthaaa <asthaa1031@gmail.com>
2026-06-07 17:36:01 +0530

    Final assignment update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from services.exlporium import OceanService
 from services.prospeo import ProspeoService
 from services.brevo import BrevoService
@@ -21,20 +22,17 @@ companies = ocean.get_similar_companies(domain)
 
 contacts = prospeo.find_decision_makers(companies)
 
-print("\nContacts Found\n")
+print("\n✅ Contacts Found\n")
 
 for contact in contacts:
 
-    print(
-        contact.name,
-        contact.company,
-        contact.email
-    )
+    print(f"Name:   {contact.name}")
+    print(f"Domain: {contact.company}")
+    print(f"Email:  {contact.email}")
+    print()
 
 
-confirm = input(
-    "\nSend Emails? (y/n): "
-)
+confirm = input("\nSend Emails? (y/n): ")
 
 if confirm.lower() == "y":
     brevo.send_emails(contacts)
